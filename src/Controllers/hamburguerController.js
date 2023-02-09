3096e1120fb670389878d4ecfea698b8430ba204
XandyNinfadora <xandriellepessanhax@gmail.com>
2023-02-08 22:17:04 -0300

    Todos os verbos http feitos,  falta column img

--- src/Controllers/hamburguerController.js
+++ src/Controllers/hamburguerController.js
@@ -1,44 +1,81 @@
 import hamburguerDAO from '../DAO/hamburguerDAO.js';
+import estoqueProduto from '../Models/estoqueProduto.js';
 class hamburguerController {
     static rotas(app) {
         // Rota para o recurso usuario
         app.get('/estoque/hamburguer', hamburguerController.listarHamburguer)
-        /*app.get('/estoque/hamburguer/:hamburguer', hamburguerController.buscarHamburguer)
+        app.get('/estoque/hamburguer/:nome_produto', hamburguerController.buscarHamburguer)
         app.post('/estoque/hamburguer', hamburguerController.adicionarHamburguer)
-        app.delete('/estoque/hamburguer/:hamburguer', hamburguerController.deletarHamburguer)
-        app.put('/estoque/hamburguer/:hamburguer', hamburguerController.atualizarHamburguer)*/
+        app.delete('/estoque/hamburguer/:id', hamburguerController.deletarHamburguer)
+        app.put('/estoque/hamburguer/:id', hamburguerController.atualizarHamburguer)
     }
 
     static async listarHamburguer(req, res) {
         const listarHamburguer = await hamburguerDAO.listarHamburguer();
         res.send(listarHamburguer);
-        console.log('ListarHamburguer foi requisitado')
+
+        console.log("ListarHamburguer foi requisitado e executado");
     }
 
-    /*
-     const listarHamburguer = await hamburguerDAO.listaHamburguer();
-        res.send(listarHamburguer);
+    static async buscarHamburguer(req, res) {
+        const hamburguerNome = await hamburguerDAO.buscarHamburguer(req.params.nome_produto);
+        if (!hamburguerNome) {
+            res.status(404).send("Nome do hamburguer não foi encontrado, tente outro");
+        }
+        else {
+            res.send(hamburguerNome);
+        }
 
-        static async buscarHamburguer(req, res){
-            res.send("RELOU FUNFOU")
-            // Console log do objeto usuario
-            console.log("até aqui GG IZI")
+        console.log("BuscarHamburguer foi requisitado e executado");
+    }
+
+    static async adicionarHamburguer(req, res) {
+        const hamburguerModel = new estoqueProduto(req.body.nome_produto, req.body.descricao_produto, req.body.valor_produto, req.body.tipo_produto);
+
+        const hamburguerAdicionado = await hamburguerDAO.adicionarHamburguer(hamburguerModel);
+
+        if (hamburguerAdicionado.erro) {
+            res.status(404).send(hamburguerAdicionado);
         }
-        static async adicionarHamburguer(req, res){
-            res.send("RELOU FUNFOU")
-            // Console log do objeto usuario
-            console.log("até aqui GG IZI")
+        else {
+            res.send(hamburguerAdicionado);
         }
-        static async deletarHamburguer(req, res){
-            res.send("RELOU FUNFOU")
-            // Console log do objeto usuario
-            console.log("até aqui GG IZI")
+
+        console.log("AdicionarHamburguer foi requisitado e executado");
+    }
+
+    static async deletarHamburguer(req, res) {
+        // Tenta deletar o usuario
+        const hamburguerDeletado = await hamburguerDAO.deletarHamburguer(req.params.id);
+        // Se o usuario não for encontrado, devolve um erro
+        if (hamburguerDeletado.erro) {
+            res.status(500).send(hamburguerDeletado);
         }
-        static async atualizarHamburguer(req, res){
-            res.send("RELOU FUNFOU")
-            // Console log do objeto usuario
-            console.log("até aqui GG IZI")
-        }*/
+        else {
+
+            res.send(hamburguerDeletado);
+        }
+
+        console.log("DeletarHamburguer foi requisitado e executado");
+    }
+
+    static async atualizarHamburguer(req, res){
+               // Preparar o usuario
+               const atualizarHamburguer = new estoqueProduto(req.body.nome_produto, req.body.descricao_produto, req.body.valor_produto, req.body.tipo_produto);
+               // Tenta atualizar o usuario
+               const hamburguerAtualizado = await hamburguerDAO.atualizarHamburguer(req.params.id, atualizarHamburguer)
+               // Se o usuario não for encontrado, devolve um erro
+               if(hamburguerAtualizado.erro){
+                   res.status(500).send(hamburguerAtualizado)
+               }
+               else {
+
+                res.send(hamburguerAtualizado)
+
+               }
+               console.log("AtualizarHamburguer foi requisitado e executado")
+            }
+           
 }
 
 
